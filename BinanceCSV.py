6d710447cbf7eee607b00cc6e669fab32d3d79c6
Adam Woo <awoox2@gmail.com>
2019-04-28 16:25:31 -0500

    Added inputs

--- BinanceCSV.py
+++ BinanceCSV.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """Binance to CSV.
 
 This module was created to pull historical data from Bininace, particularaly as a training set
@@ -37,10 +38,12 @@ import datetime as dt
 def interval_to_milliseconds(interval):
     """Convert a Binance interval string to milliseconds
 
-    Args:
+    Parameters
+    ----------
         interval (str): Binance interval string 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w
 
-    Returns:
+    Returns
+    -------
         int: interval in millisecond format.
     """
     ms = None
@@ -65,10 +68,12 @@ def date_to_milliseconds(date_str):
     If using offset strings add "UTC" to date string e.g. "now UTC", "11 hours ago UTC"
     See dateparse docs for formats http://dateparser.readthedocs.io/en/latest/
 
-    Args:
+    Parameters
+    ----------
         date_str (str): A date.
 
-    Returns:
+    Returns
+    -------
         Dataframe: A date in millisecond format.
     """
     # get epoch value in UTC
@@ -83,32 +88,35 @@ def date_to_milliseconds(date_str):
     return int((d - epoch).total_seconds() * 1000.0)
 
 
-def get_bars(symbol, interval='1h', start_date="January 01, 2018"):
+def get_bars(symbol, interval='1h', start_date="January 01, 2018", end_date="January 01, 2020"):
     """Query the Binanace API starting from a given date.
 
-    Args:
+    Parameters
+    ----------
         symbol (str): The first parameter.
         interval (str): The second parameter.
         start_date (str): Starting date.
 
-    Returns:
+    Returns
+    -------
         Dataframe: Pandas dataframe of historical info
     """
     timeframe = interval_to_milliseconds(interval)
     limit = 500
     root_url = 'https://api.binance.com/api/v1/klines'
     start_ts = date_to_milliseconds(start_date)
+    end_ts = date_to_milliseconds(end_date)
     data = []
     idx = 0
     while True:
-        url = root_url + '?symbol=' + symbol + '&interval=' + interval + '&startTime=' + str(start_ts)
+        url = root_url + '?symbol=' + symbol + '&interval=' + interval + '&startTime=' + str(start_ts) + '&endTime=' + str(end_ts)
         temp_data = json.loads(requests.get(url).text)
         if data:
             data += temp_data
         else:
             data = temp_data
-        start_ts = temp_data[len(temp_data) - 1][0] + timeframe
-        if len(temp_data) < limit:
+        if(len(temp_data)): start_ts = temp_data[len(temp_data) - 1][0] + timeframe
+        if (len(temp_data) < limit):
             break
         idx += 1
         if idx % 5 == 0:
@@ -126,21 +134,24 @@ if __name__ == '__main__':
     try:
         months = mdates.MonthLocator()
         days = mdates.DayLocator()
-        trading_pair = 'BNBUSDT'
-        interval = '4h'
+        trading_pair = input("Trading Pair: ").upper()
+        interval = input("Interval: ").lower()
+        start_date = input("Start Date (DD/MM/YY'): ")
+        end_date = input("End Date (DD/MM/YY'): ")
+        filename = input("Filename: ")
         directory = "./Data/"+trading_pair
-        data = get_bars(trading_pair, interval)
+        data = get_bars(trading_pair, interval, start_date, end_date)
         if not os.path.exists(directory):
             os.makedirs(directory)
-        data.to_csv(directory+"/"+interval+trading_pair+".csv", sep='\t', encoding='utf-8')
+        data.to_csv(directory+"/"+filename+".csv", sep='\t', encoding='utf-8')
 
         plot = data['c'].astype('float').plot(figsize=(16, 9))
         plt.xlabel("Time: " + interval)
-        plt.ylabel(trading_pair)
+        plt.title(trading_pair)
         plot.xaxis.set_major_locator(months)
         plot.xaxis.set_minor_locator(days)
         plot.grid(True)
-        plt.savefig(directory+"/"+interval+trading_pair+'.png')
+        plt.savefig(directory+"/"+filename+'.png')
     
     except KeyboardInterrupt:
         sys.exit()
---
+++ README.md
@@ -0,0 +1,9 @@
+A script for pulling Binance historical data and storing it as a CSV
+
+1. Download the script
+2. Run the script
+3. Follow the console instructions
+
+TODO:
+1. validation
+2. error handling
